fda746577dca367bfa68abe88cab52285fb3b310
Martin Patino <mpatino117@gmail.com>
2025-05-30 17:01:25 -0700

    Refactor Reddit Scout agents: consolidate functionality into async_reddit_scout, remove redundant files

--- agents/async_reddit_scout/__init__.py
+++ agents/async_reddit_scout/__init__.py
@@ -1,1 +1,5 @@
-from .agent import agent
+# This file makes 'reddit_scout' a Python package.
+# It should import the agent instance to make it discoverable.
+
+# Import the agent instance (using conventional 'root_agent' name)
+from .agent import agent 
--- agents/reddit_scout/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file makes 'reddit_scout' a Python package.
-# It should import the agent instance to make it discoverable.
-
-# Import the agent instance (using conventional 'root_agent' name)
-from .agent import agent 
